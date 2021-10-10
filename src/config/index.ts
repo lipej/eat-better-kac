ebffc68a3f5a44cef56f0db0303f09783c4c38bd
Felipe José <lipejose@icloud.com>
2021-10-10 02:13:01 -0300

    refactor: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,30 +1,30 @@
-require("dotenv").config();
+require('dotenv').config()
 
 export enum NodeEnv {
-  PRODUCTION = "production",
-  DEVELOPMENT = "development",
+  PRODUCTION = 'production',
+  DEVELOPMENT = 'development'
 }
 
 interface IConfig {
-  env: NodeEnv;
+  env: NodeEnv
   server: {
-    host: string;
-    port: number;
-  };
+    host: string
+    port: number
+  }
   jwt: {
-    secret: string;
-    expires: number;
-  };
+    secret: string
+    expires: number
+  }
 }
 
 export const Config: IConfig = {
   env: process.env.NODE_ENV as NodeEnv,
   server: {
-    host: process.env.HOST || "127.0.0.1",
-    port: Number(process.env.APP_PORT) || 3000,
+    host: process.env.HOST || '127.0.0.1',
+    port: Number(process.env.APP_PORT) || 3000
   },
   jwt: {
-    secret: process.env.JWT_SECRET || "secret_test",
-    expires: Number(process.env.JWT_EXPIRATION) || 4,
-  },
-};
+    secret: process.env.JWT_SECRET || 'secret_test',
+    expires: Number(process.env.JWT_EXPIRATION) || 4
+  }
+}
