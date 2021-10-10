0f4659020a4fffa28bc664cf8d0b9cf62ae990db
Felipe José <lipejose@icloud.com>
2021-10-10 19:25:51 -0300

    refactor: config

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 export enum NodeEnv {
   PRODUCTION = 'production',
-  DEVELOPMENT = 'development'
+  DEVELOPMENT = 'development',
+  TEST = 'testing'
 }
 
 interface IConfig {
@@ -18,7 +19,7 @@ interface IConfig {
 }
 
 export const Config: IConfig = {
-  env: process.env.NODE_ENV as NodeEnv,
+  env: process.env.APP_ENV as NodeEnv,
   server: {
     host: process.env.HOST || '127.0.0.1',
     port: Number(process.env.APP_PORT) || 3000
