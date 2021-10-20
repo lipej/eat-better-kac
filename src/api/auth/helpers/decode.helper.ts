50e568b612a719acf797c52f79b8d02ec258bfde
Felipe José <lipejose@icloud.com>
2021-10-19 22:14:35 -0300

    feat: role helper

--- src/api/auth/helpers/decode.helper.ts
+++
@@ -1,9 +0,0 @@
-import jwt_decode from 'jwt-decode'
-
-const decodeToken = (token: string) => jwt_decode(token)
-
-export const getIdFromToken = (token): number => {
-  const decoded: any = decodeToken(token)
-
-  return Number(decoded.id)
-}
--- src/api/auth/index.ts
+++ src/api/auth/index.ts
@@ -1,4 +1,4 @@
 export * from './effects/login.effect'
-export * from './helpers/decode.helper'
 export * from './helpers/token.helper'
 export * from './middleware/auth.middleware'
+export * from './helpers/role.helper'
