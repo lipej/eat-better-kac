9573ee2614f3ff4d6e16879b5c453233a794d6f0
Felipe José <lipejose@icloud.com>
2021-10-19 22:12:23 -0300

    feat: new search util

--- src/api/recipes/utils/create-search.util.ts
+++ src/api/recipes/utils/create-search.util.ts
@@ -1,13 +1,31 @@
-export const mountSearch = (search: string) => {
+export const mountSearch = (searchQuery: string) => {
+  if (!searchQuery) return { published: true, deletedAt: null }
+
   const contains: string[] = []
   const notContains: string[] = []
 
-  search.split(',').forEach((value) => {
+  searchQuery.split(',').forEach((value) => {
     if (value[0] === '!') notContains.push(value.replace('!', ''))
     else contains.push(value)
   })
 
-  return `${contains.map((value) => `'${value}'`).join(' | ')} & (${notContains
-    .map((value) => `!'${value}'`)
-    .join(' & ')})`
+  const withContains = contains.length
+    ? `${contains.map((value) => `'${value}'`).join(' | ')}`
+    : false
+
+  const withNotContains = notContains.length
+    ? `(${notContains.map((value) => `!'${value}'`).join(' & ')})`
+    : false
+
+  const hasBoth = withContains && withNotContains ? `${withContains} & ${withNotContains}` : false
+
+  const search = hasBoth ? hasBoth : withContains || withNotContains
+
+  return {
+    title: { search },
+    time: { search },
+    ingredients: { search },
+    published: true,
+    deletedAt: null
+  }
 }
