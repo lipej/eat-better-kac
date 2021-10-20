e1b950134e314039e4275f92d4d3512eb487c277
Felipe José <lipejose@icloud.com>
2021-10-19 22:13:58 -0300

    refactor: adjusts in auth middleware

--- src/api/auth/middleware/auth.middleware.ts
+++ src/api/auth/middleware/auth.middleware.ts
@@ -3,13 +3,16 @@ import { of, map, mergeMap } from 'rxjs'
 
 import { Config } from '@config'
 import { Users } from '@users'
+import { User } from '@prisma/client'
 
 const config = { secret: Config.jwt.secret }
 
 const verifyPayload$ = (payload: { id: string }) =>
   of(payload).pipe(
-    map((payload) => parseInt(payload.id)),
-    mergeMap(Users.validate)
+    map((payload) => Number(payload.id)),
+    mergeMap(Users.validate),
+    map((user) => user as User),
+    map((user) => ({ id: user.id, role: user.role }))
   )
 
 export const authorize$ = jwt$(config, verifyPayload$)
