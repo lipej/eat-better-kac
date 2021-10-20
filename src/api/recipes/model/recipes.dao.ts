59acbab91c0ee2a9f9245f78c21592622ea3f70f
Felipe José <lipejose@icloud.com>
2021-10-19 22:12:47 -0300

    feat: recipe model

--- src/api/recipes/model/recipes.dao.ts
+++ src/api/recipes/model/recipes.dao.ts
@@ -1,9 +1,9 @@
 import { prisma } from '@prisma-main'
 import { Recipe } from '@prisma/client'
-import { from, map, Observable } from 'rxjs'
-import { HttpError } from '@marblejs/http'
+import { from, map, mergeMap, Observable } from 'rxjs'
 
 import { mountSearch } from '@recipes'
+import { HttpError } from '@marblejs/http'
 
 type IQuery = {
   page: number
@@ -22,8 +22,6 @@ export const Recipes = {
     return from(prisma.recipe.findUnique({ where: { id } })).pipe(
       map((recipe) => {
         if (!recipe) throw new HttpError('Recipe not found', 404)
-        if (!recipe.published) throw new HttpError('Can not delete a recipe not published', 400)
-        if (recipe.deletedAt) throw new HttpError('Recipe already deleted', 400)
 
         return recipe
       })
@@ -32,37 +30,50 @@ export const Recipes = {
 
   showMany(query: IQuery) {
     const search = mountSearch(query.search)
-
+    const page = query.page ? Number(query.page - 1) : 0
+    const limit = query.limit ? Number(query.limit) : 10
     return from(
       prisma.recipe.findMany({
-        where: {
-          title: { search },
-          time: { search },
-          ingredients: { search }
+        where: search as any,
+        include: {
+          author: { select: { name: true } }
         },
-        include: { author: true, photos: true },
-        take: query.limit,
-        skip: query.limit * query.page
+        take: limit,
+        skip: limit * page
       })
     ).pipe(
       map((result) => ({
         result,
-        limit: query.limit,
-        page: query.page === 0 ? 1 : query.page - 1
+        limit: limit,
+        page: page === 0 ? 1 : page + 1
       }))
     )
   },
 
-  remove(id: number) {
-    return from(prisma.recipe.update({ where: { id }, data: { deletedAt: new Date() } }))
+  remove(data: { id: number }) {
+    return from(prisma.recipe.findUnique({ where: { id: data.id } })).pipe(
+      map((recipe) => {
+        if (!recipe) throw new HttpError('Recipe not found', 404)
+        return recipe.id
+      }),
+      mergeMap((id) =>
+        prisma.recipe.update({ where: { id }, data: { deletedAt: new Date(), published: false } })
+      )
+    )
   },
 
-  update(data: { id: number; fields: Omit<IRecipeData, 'authorId'> & { published: boolean } }) {
-    return from(
-      prisma.recipe.update({
-        where: { id: data.id },
-        data: { ...data.fields, updatedAt: new Date() }
-      })
+  update(data: { id: number; fields: Partial<IRecipeData> & { published: boolean } }) {
+    return from(prisma.recipe.findUnique({ where: { id: data.id } })).pipe(
+      map((recipe) => {
+        if (!recipe) throw new HttpError('Recipe not found', 404)
+        return recipe.id
+      }),
+      mergeMap((id) =>
+        prisma.recipe.update({
+          where: { id },
+          data: { ...data.fields, updatedAt: new Date(), published: data.fields.published || false }
+        })
+      )
     )
   }
 }
